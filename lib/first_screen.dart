006a9c8489ff7f8ef083b04d5c2f767d5573d3ec
HatemReda7 <hreda2567@gmail.com>
2023-09-27 20:01:49 +0300

    created 3 screens

---
+++ lib/first_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  static const String routeName= "first Screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("testing",),
+      ),
+    );
+  }
+}
---
+++ lib/second_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  static const String routeName="Second Screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("testing",),
+      ),
+    );
+  }
+}
